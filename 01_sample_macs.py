95788f972e14cb4543dee0f67ea52f0b1dfeb767
Kenenbek R <kenenbek@gmail.com>
2024-07-25 17:50:29 +0600

    f

--- 01_sample_macs.py
+++ 01_sample_macs.py
@@ -211,7 +211,7 @@ if GENERATE_SAMPLES==True:
         print("\t... saving base cobra model: ", ENSEMBLE_BASEMODEL_FILE)
         save_json_model(COBRA_MODEL,ENSEMBLE_BASEMODEL_FILE)
         
-    base_flux_samples = clean_flux_samples(pd.read_csv(MODEL_SAMPLES_FILE,index_col=0))
+    base_flux_samples = clean_flux_samples(COBRA_MODEL, pd.read_csv(MODEL_SAMPLES_FILE,index_col=0))
 
     ### Create Species object
     SPECIES_MODEL = cs.Species("species_obj")
